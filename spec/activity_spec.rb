1f35a9120f0a986bc75da69fda03e6652ef48543
Matt Kragen <mattdkragen@gmail.com>
2021-06-07 09:32:13 -0700

    Create test for total cost

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -2,11 +2,11 @@ require './lib/activity'
 require 'rspec'
 
 RSpec.describe Activity do
-  before :all do
-    @activity = Activity.new("Brunch")
-  end
-
   describe 'instantiation' do
+    before :each do
+      @activity = Activity.new("Brunch")
+    end
+    
     it 'exists' do
       expect(@activity).to be_a(Activity)
     end
@@ -18,9 +18,22 @@ RSpec.describe Activity do
   end
 
   describe 'methods' do
+    before :each do
+      @activity = Activity.new("Brunch")
+    end
+    
     it 'adds participants to activity' do
       @activity.add_participant("Maria", 20)
       expect(@activity.participants).to eq({"Maria" => 20})
+      @activity.add_participant("Luther", 40)
+      expect(@activity.participants).to eq({"Maria" => 20, "Luther" => 40})
+    end
+
+    it 'returns total cost' do
+      @activity.add_participant("Maria", 20)
+      expect(@activity.total_cost).to eq(20)
+      @activity.add_participant("Luther", 40)
+      expect(@activity.total_cost).to eq(60)
     end
   end
 end
