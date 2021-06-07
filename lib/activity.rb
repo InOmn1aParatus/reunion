7e49f397d5ce0a062afb7ed27dd667abb1e1c86e
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 22:36:27 -0700

    Make #add_participant method

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,8 +1,12 @@
 class Activity
   attr_reader :name, :participants
-  
+
   def initialize(name)
     @name = name
     @participants = {}
+  end
+
+  def add_participant(name, cost)
+    @participants[name] = cost
   end
 end
