e21ced8e34088ba5bcbdf65df9fdd31cdf0c4178
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 21:24:27 -0700

    Create test for adding participants

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -2,16 +2,25 @@ require './lib/activity'
 require 'rspec'
 
 RSpec.describe Activity do
+  before :all do
+    @activity = Activity.new("Brunch")
+  end
+
   describe 'instantiation' do
     it 'exists' do
-      activity = Activity.new("Brunch")
-      expect(activity).to be_a(Activity)
+      expect(@activity).to be_a(Activity)
     end
 
     it 'has attributes' do
-      activity = Activity.new("Brunch")
-      expect(activity.name).to eq("Brunch")
-      expect(activity.participants).to eq({})
+      expect(@activity.name).to eq("Brunch")
+      expect(@activity.participants).to eq({})
+    end
+  end
+
+  describe 'methods' do
+    it 'adds participants to activity' do
+      @activity.add_participant("Maria", 20)
+      expect(@activity.participants).to eq({"Maria" => 20})
     end
   end
 end
