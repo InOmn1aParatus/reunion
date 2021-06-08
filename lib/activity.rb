86966dad7a263027860296c6f44cb8272c584133
Matt Kragen <mattdkragen@gmail.com>
2021-06-07 20:10:26 -0700

    Finish test for #owed

--- lib/activity.rb
+++ lib/activity.rb
@@ -17,4 +17,12 @@ class Activity
   def split
     total_cost / 2
   end
+
+  def owed
+    amt_owed = {}
+    @participants.each do |name, cost|
+      amt_owed[name] = split - cost
+    end
+    amt_owed
+  end
 end
